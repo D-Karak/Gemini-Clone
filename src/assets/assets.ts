c7857832c3f299ab2a20060998a381dfc571e4dd
D-Karak <dibyendu.karak05@gmai.com>
2026-02-12 18:54:08 +0530

    initial first commit

---
+++ src/assets/assets.ts
@@ -0,0 +1,27 @@
+import menu_icon from "./menu_icon.png";
+import plus_icon from "./plus_icon.png";
+import gemini_icon from "./gemini_logo.png"
+import history_icon from "./history_icon.png"
+import home_icon from "./home_icon.png"
+import settings_icon from "./settings_icon.png"
+import bulb_icon from "./bulb_icon.png"
+import compass_icon from "./compass_icon.png"
+import gallery_icon from "./gallery_icon.png"
+import mic_icon from "./mic_icon.png"
+import send_icon from "./send_icon.png"
+import user_icon from "./user_icon.png"
+
+export const assets = {
+    menu_icon,
+    plus_icon,
+    gemini_icon,
+    history_icon,
+    home_icon,
+    settings_icon,
+    bulb_icon,
+    compass_icon,
+    gallery_icon,
+    mic_icon,
+    send_icon,
+    user_icon
+}
